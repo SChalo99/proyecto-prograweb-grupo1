b631be8a6b178eba9875317ce7b522954e4a4ae8
SChalo99 <gonzalogarma@hotmail.com>
2022-10-11 19:27:40 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import './App.css';
 import { Container, Row } from 'react-bootstrap';
 import OptimizePage from './components/Pages';
-import SideBar from './components/SideMenu/SideBar'; 
+import SideBar from './components/SideMenu/SideBar';
 import ListProduct from './components/List';
 import CommonQuestions from './components/Questions';
 import SupportGuide from './components/SupportGuides';
-import RankingPeriferic from './components/Ranking' 
+import RankingPeriferic from './components/Ranking'
 import Opciones from './components/Opciones';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Row>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={SideBar} />
-              <Route path="productos" element={ListProduct} />
-              <Route path="preguntas" element={CommonQuestions} />
-              <Route path="opciones" element={Opciones} />
-              <Route path="support" element={SupportGuide} />
-              <Route path="ranking" element={RankingPeriferic} />
-              <Route path="optimized" element={OptimizePage}/>
-              <Route path="*" />
-            </Routes>
-          </BrowserRouter>
-        </Row>
-      </Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SideBar />} />
+          <Route path="productos" element={<ListProduct />} />
+          <Route path="preguntas" element={<CommonQuestions />} />
+          <Route path="opciones" element={<Opciones />} />
+          <Route path="support" element={<SupportGuide />} />
+          <Route path="ranking" element={<RankingPeriferic />} />
+          <Route path="optimized" element={<OptimizePage />} />
+          <Route path="*" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
